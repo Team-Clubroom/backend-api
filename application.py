6650138251e3fee1582e851eaf0cf58268aab6da
bkhuckaby <bkhuckaby@ualr.edu>
2023-09-15 21:32:53 -0500

    Added email register/write to db.users

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from email_validator import validate_email, EmailNotValidError
 from flask import Flask, request, jsonify
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from flask_sqlalchemy import SQLAlchemy
@@ -11,6 +12,8 @@ jwt = JWTManager(application)
 db = SQLAlchemy(application)
 # db.init_app(application)
 bcrypt = Bcrypt(application)
+
+
 # login_manager = LoginManager(application)
 # login_manager.login_view = 'login'
 
@@ -24,6 +27,14 @@ class Employer(db.Model):
     employer_dissolved_date = db.Column(db.String(10))
     employer_status = db.Column(db.String(255))
     employer_legal_status = db.Column(db.String(255))
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    email_address = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    access_permissions = db.Column(db.Integer)
 
 
 @application.route('/')
@@ -43,8 +54,53 @@ def hello_world_private():
     return "Hello world privately :)"
 
 
-@application.route('/login', methods=['POST'])
+@application.route('/register', methods=['POST'])
 def register_user():
+    try:
+        data = request.json
+        email = data.get("email", None)
+        password = data.get("password", None)
+
+        if not email or not password:
+            return jsonify({
+                "error": "Required arguments missing"
+            }), 400
+        emailinfo = validate_email(email, check_deliverability=False)
+        email = emailinfo.normalized
+
+        # TODO: Exception for user already registered
+
+        user = User()
+        user.email_address = email
+        user.password = password
+        user.access_permissions = None
+
+        db.session.add(user)
+        db.session.commit()
+
+        # TODO: Send verification email here
+
+        success_response = {
+            'message': 'Verification email sent'
+        }
+
+        return jsonify(success_response), 201
+
+    except EmailNotValidError as e:
+        error_response = {
+            'error': 'Invalid email'
+        }
+        return jsonify(error_response), 400
+
+    except RuntimeError:
+        error_response = {
+            'error': 'Invalid request format'
+        }
+        return jsonify(error_response), 400  # 400 Bad Request status code
+
+
+@application.route('/login', methods=['POST'])
+def login_user():
     try:
         # Get data from the JSON request
         data = request.json
@@ -55,7 +111,7 @@ def register_user():
 
         if email != password:
             return jsonify({
-                "error": "Password or email are incorrect"
+                'error': 'Password or email are incorrect'
             }), 400
 
         # create token
